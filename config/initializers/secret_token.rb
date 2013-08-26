09005586c494e37b4c3cf6d355f0506a0a88b2db
Tony Huang <ttonyhh@gmail.com>
2013-08-25 17:22:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accelsafety::Application.config.secret_token = '70e40cbba699d2e8aa19ca1f0022fbffd7c8d557cfa6c0364a94c8eb3125233a052a263668706a2dc2d32ef18b3637f2596f643bd641c6af7c052bc2db07c571'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Accelsafety::Application.config.session_store :cookie_store, key: '_accelsafety_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Accelsafety::Application.config.session_store :active_record_store
